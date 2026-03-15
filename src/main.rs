466365ada838c850c7e756c55fa9b9b88681228b
김영현 <kyh04034@gmail.com>
2026-03-15 21:22:27 +0900

    코드 변경 테스트

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use core::num;
+
 fn main() {
     let number = 7;
-    
+    let num = 3;
     if number > 5 {
         println!("5보다 큼");
     } else {
         println!("5보다 작거나 같음");
+        print!("{}는 5보다 작거나 같음", number);
     }
 }
     
